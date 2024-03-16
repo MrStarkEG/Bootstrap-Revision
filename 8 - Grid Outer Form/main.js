498224f82ae2c140a4024ec57783d7bfc662fc1a
Ahmed Elshahat <ahmedelking74044@gmail.com>
2024-03-16 02:54:37 +0200

    initial commit

---
+++ 8 - Grid Outer Form/main.js
@@ -0,0 +1,8 @@
+const tooltipTriggerList = [].slice.call(document.querySelectorAll(".tt"));
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl);
+// });
+
+tooltipTriggerList.forEach((t) => {
+  new bootstrap.Tooltip(t);
+});
